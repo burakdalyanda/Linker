1ec067ea67dc5e41f73a3a152f8e96cf828788fb
Burak Dalyanda <burak.dalyanda@hotmail.com>
2023-01-10 19:24:13 +0300

    Config Files

---
+++ config/seo/home/show.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        'property' => 'og:url',
+        'content' =>  '{{URL.BASE}}',
+    ],
+    [
+        'property' => 'og:title',
+        'content' => 'Linker - URL Shortener.',
+    ],
+    [
+        'property' => 'og:description',
+        'content' => 'Shorten, create and share trusted, powerful links for your business. Try Linker for free.',
+    ],
+    [
+        'property' => 'keywords',
+        'content' => 'url shortener, link shortener'
+    ],
+    [
+        'property' => 'og:type',
+        'content' => 'website',
+    ],
+];
